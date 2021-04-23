ce7fa15c70dee6c89b849adbc8b1512282032283
Marco Martin <notmart@gmail.com>
2021-04-23 14:29:00 +0200

    install plasmoi plugins in new folder

--- tools/migratedataengines.sh
+++ tools/migratedataengines.sh
@@ -8,7 +8,17 @@ find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/Plasma(\/|::)DataEn
 find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/Plasma(\/|::)Service/Plasma5Support\\1Service/g"
 find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/Plasma(\/|::)ServiceJob/Plasma5Support\\1ServiceJob/g"
 
-find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/namespace Plasma\n/namespace Plasma5Support\n/g"
+find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/namespace Plasma(;|\n)/namespace Plasma5Support\\1/g"
+
+
+
+find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/plasma\/datacontainer/plasma5support\/datacontainer/g"
+find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/plasma\/dataengine/plasma5support\/dataengine/g"
+find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/plasma\/dataengineconsumer/plasma5support\/dataengineconsumer/g"
+
+find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/plasma\/service/plasma5support\/service/g"
+find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/plasma\/servicejob/plasma5support\/servicejob/g"
+
 
 find . -name "*.h" -o -name "*.cpp" | xargs perl -p -i -e "s/K_EXPORT_PLASMA_DATAENGINE_WITH_JSON/K_EXPORT_PLASMA5SUPPORT_DATAENGINE_WITH_JSON/g"
 
@@ -17,3 +27,5 @@ find . -name "*.desktop" | xargs perl -p -i -e "s/Plasma\/DataEngine/Plasma5Supp
 
 # CMake
 find . -name "CMakeLists.txt"  | xargs perl -p -i -e "s/KF5::Plasma/KF5::Plasma5Support/g"
+find . -name "CMakeLists.txt"  | xargs perl -p -i -e "s/PLASMA_DATA_INSTALL_DIR/PLASMA5SUPPORT_DATA_INSTALL_DIR/g"
+find . -name "CMakeLists.txt"  | xargs perl -p -i -e "s/plasma\/dataengine/plasma5support\/dataengine/g"
