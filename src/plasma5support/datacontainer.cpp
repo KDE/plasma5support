43c549a37bd9d3dee798bef28e70eb37adb4b628
Marco Martin <notmart@gmail.com>
2023-03-02 11:37:27 +0100

    Don't erase an end iterator
    port of https://invent.kde.org/frameworks/plasma-framework/-/merge_requests/743

--- src/plasma5support/datacontainer.cpp
+++ src/plasma5support/datacontainer.cpp
@@ -327,9 +327,9 @@ void DataContainer::disconnectVisualization(QObject *visualization)
                 disconnect(this, SIGNAL(modelChanged(QString, QAbstractItemModel *)), visualization, SLOT(modelChanged(QString, QAbstractItemModel *)));
             }
         }
+        d->relayObjects.erase(objIt);
     }
 
-    d->relayObjects.erase(objIt);
     d->checkUsage();
 }
 
